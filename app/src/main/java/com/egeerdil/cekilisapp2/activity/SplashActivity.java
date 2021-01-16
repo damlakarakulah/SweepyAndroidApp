894ce148bf66a06b33cff0da3bbe951314741a0f
damla.karakulah <damla.karakulah@kolaygelsin.com>
2021-01-17 01:11:20 +0300

    firebase token refresh

--- app/src/main/java/com/egeerdil/cekilisapp2/activity/SplashActivity.java
+++ app/src/main/java/com/egeerdil/cekilisapp2/activity/SplashActivity.java
@@ -3,12 +3,18 @@ package com.egeerdil.cekilisapp2.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.egeerdil.cekilisapp2.R;
+import com.egeerdil.cekilisapp2.db.ServiceConfig;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 
 public class SplashActivity extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGTH = 500;
@@ -25,11 +31,23 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
                 if(firebaseUser != null && firebaseUser.isEmailVerified()) {
-                    Intent mainIntent = new Intent(SplashActivity.this, StartActivity.class);
-                    SplashActivity.this.startActivity(mainIntent);
-                    SplashActivity.this.finish();
+                    firebaseUser.getIdToken(true).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<GetTokenResult> task) {
+                            ServiceConfig.Token = task.getResult().getToken();
+                            if(ServiceConfig.Token == null){
+                                Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                                SplashActivity.this.startActivity(mainIntent);
+                                SplashActivity.this.finish();
+                            }
+                            else {
+                                Intent mainIntent = new Intent(SplashActivity.this, StartActivity.class);
+                                SplashActivity.this.startActivity(mainIntent);
+                                SplashActivity.this.finish();
+                            }
+                        }
+                    });
                 }
                 else {
                     Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
